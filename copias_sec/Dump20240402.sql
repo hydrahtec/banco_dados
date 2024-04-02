8241f902f41ec95b4298ac387764928f28f6915c
Eliel Silva <hydrahtec@gmail.com>
2024-04-02 13:33:20 -0300

    copias de sugurança
    gerenciando copias, export and import

---
+++ copias_sec/Dump20240402.sql
@@ -0,0 +1,91 @@
+CREATE DATABASE  IF NOT EXISTS `cadastro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cadastro`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: cadastro
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `curso`
+--
+
+DROP TABLE IF EXISTS `curso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `curso` (
+  `idcurso` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `descrcao` text,
+  `carga` int unsigned DEFAULT NULL,
+  `totaulas` int DEFAULT NULL,
+  `ano` year DEFAULT '2016',
+  PRIMARY KEY (`idcurso`),
+  UNIQUE KEY `nome` (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `curso`
+--
+
+LOCK TABLES `curso` WRITE;
+/*!40000 ALTER TABLE `curso` DISABLE KEYS */;
+/*!40000 ALTER TABLE `curso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pessoa`
+--
+
+DROP TABLE IF EXISTS `pessoa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pessoa` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `profissao` varchar(40) DEFAULT 'teste',
+  `nascimento` date DEFAULT NULL,
+  `sexo` enum('M','F') DEFAULT NULL,
+  `peso` decimal(5,2) DEFAULT NULL,
+  `altura` decimal(3,2) DEFAULT NULL,
+  `nacionalidade` varchar(20) DEFAULT 'Brasil',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pessoa`
+--
+
+LOCK TABLES `pessoa` WRITE;
+/*!40000 ALTER TABLE `pessoa` DISABLE KEYS */;
+INSERT INTO `pessoa` VALUES (1,'Godofredo',NULL,'1984-01-02','M',130.80,1.83,'Brasileiro'),(2,'Maria divina',NULL,'2000-05-29','F',75.40,1.68,'Americana'),(3,'Carlos alexndre',NULL,'2000-07-08','M',89.80,1.72,'Japones'),(4,'Rosa Ana',NULL,'2000-09-15','F',65.30,1.55,'Russa');
+/*!40000 ALTER TABLE `pessoa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'cadastro'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-02 13:31:21
--- curso_em_video_MySQL.sql
+++ curso_em_video_MySQL.sql
@@ -2,9 +2,7 @@
 
 create database cadastro
 default character set utf8mb4
-default collate utf8mb4_general_ci;
-
-*/
+default collate utf8mb4_general_ci;*/
 
 /*-- criação da tabela pessoas
 create table pessoas (
@@ -16,8 +14,7 @@ peso DECIMAL(5,2),
 altura DECIMAL(3,2),
 nacionalidade VARCHAR(20) DEFAULT 'Brasil',
 primary key (id)
-) default charset = utf8mb4;
-*/
+) default charset = utf8mb4;*/
 
 /*-- povoamento da tabela pessoazs
 
@@ -27,8 +24,8 @@ values
 ('1', 'godofredo', '1984-01-02', 'M','78.5', '1.83', default),
 (default, 'Maria divina', '2000-05-29', 'M','75.4', '1.68', 'Americano'),
 (default, 'Carlos alexndre', '2000-07-08', 'M','89.8', '1.72', 'Japonesa'),
-(default, 'Rosa Ana', '2000-09-15', 'M','130.2', '1.55', 'Russa');
-*/
+(default, 'Rosa Ana', '2000-09-15', 'M','130.2', '1.55', 'Russa');*/
+
 
 /* -- updates realizados 
 
@@ -38,17 +35,14 @@ update pessoas set nacionalidade = 'Japones' where id = 3;
 update pessoas set nacionalidade = 'Brasileiro' where id = 1;
 update pessoas set peso = '65.3' where id = 4;
 update pessoas set peso = '130.8' where id = 1;
-update pessoas set nome = 'Godofredo' where id = 1; */
+update pessoas set nome = 'Godofredo' where id = 1;*/
 
 /*--alterando a estrutura da tabela pessoas
 alter table pessoas add column profissao varchar(10);
 alter table pessoas drop column profissao;
 alter table pessoas add column profissao varchar(30) after nome;
--Se quiser inserir a nova coluna na primeira posição basta 
-trocar 'after nome' por 'first';
-alter table pessoas modify column profissao varchar(40) default 'teste'
-alter table pessoas rename to pessoa;
-;*/
+alter table pessoas modify column profissao varchar(40) default 'teste';
+alter table pessoas rename to pessoa;*/
 
 /*-- criação da tabela curso
 create table if not exists curso (
@@ -65,9 +59,8 @@ alter table curso add primary key (idcurso);
 alter table curso modify column idcurso int not null auto_increment;
 */
 
-
-
 desc curso
+
 
 
 
